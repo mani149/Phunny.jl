b13fdd99c41eeaa02ba0cc163cb3874bae547230
Documenter.jl <documenter@juliadocs.github.io>
2025-09-26 02:15:07 +0000

    build based on 164e53d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Phunny","page":"Home","title":"Phunny","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Phunny.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
